22801a4ac166c56de63b2e7b54669a29baaf8839
Amina <mamikami@yandex.ru>
2017-08-16 10:34:52 +0300

    added statistics cloud

---
+++ js/stat.js
@@ -0,0 +1,53 @@
+'use strict';
+
+window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.strokeRect(110, 20, 420, 270);
+  ctx.fillRect(110, 20, 420, 270);
+
+  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
+  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270);
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
+
+  var max = -1;
+  var maxIndex = -1;
+
+  for (var i = 0; i < times.length; i++) {
+    var time = times[i];
+    if (time > max) {
+      max = time;
+      maxIndex = i;
+    }
+  }
+
+  var histogramHeight = 150; // px;
+  var step = histogramHeight / (max - 0); // px;
+  console.log(step);
+
+  // ctx.fillText('Худшее время: ' + max.toFixed(2) + 'мс у игрока ' + names[maxIndex], 120, 60);
+  console.log('Худшее время: ', max, 'мс у игрока ', names[maxIndex]);
+
+  var barHeigth = 150; // px;
+  var indent = 50; // px;
+  var initialX = 120; // px;
+  var initialY = 80; // px;
+
+  ctx.textBaseline = 'top'; // положение надписи от левого верхнего угла
+
+  ctx.fillRect(initialX, initialY, barHeigth, times[0] * step);
+  ctx.fillText(names[0], initialX, initialY + histogramHeight);
+
+  // for (i = 0; i < times.length; i++) {
+  //   ctx.fillRect(initialX + indent * i, initialY, times[i] * step, barHeigth);
+  //   ctx.fillText(names[i], initialX + indent * i, initialY + histogramWidth);
+  // }
+
+};
+
+
